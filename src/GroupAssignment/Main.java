32d1c859aa93b2077258b4eefd10087f6c61e584
Scorpse <lyminhphucsupreme1010@gmail.com>
2023-08-27 17:23:57 +0700

    Change main

--- src/GroupAssignment/Main.java
+++ src/GroupAssignment/Main.java
@@ -4,7 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Port port1 = new Port(3976250, "PhucLy", 25.94F,  4.73F, 5000F, true);
         Port port2 = new Port(3976251, "Phuc", 36.95F,  40.23F, 3000F, false);
+        Port port3 = new Port(3976251, "Phuc", 36.5F,  40.1F, 4200F, true);
         System.out.println(port1);
         System.out.println(port2);
+        System.out.println(port3.getStoringCapacity());
     }
 }
